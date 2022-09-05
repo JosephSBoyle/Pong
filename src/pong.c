4ffa27ec9746bdcd1e58d2668143b7d24662a5ca
Joe Boyle <joe.boyle@blackcowtech.uk>
2022-09-05 18:39:43 +0100

    refactor

--- src/pong.c
+++ src/pong.c
@@ -1,25 +1,33 @@
 #include "raylib.h"
 
+// Screen configuration
+const int FRAME_HEIGHT = 480;
+const int FRAME_WIDTH  = 640;
+const int TARGET_FPS = 60;
+// Ball configuration
+const int DEFAULT_BALL_X_POS = FRAME_WIDTH / 2;
+const int DEFAULT_BALL_Y_POS = 20;
+// Difficulty
+const int DEFAULT_BALL_X_VEL = 6;
+const int DEFAULT_BALL_Y_VEL = 6;
+
+
 int main()
 {
-    // Screen configuration
-    const int height = 480;
-    const int width  = 640;
-    const int target_fps = 60;
-
     // Ball variables
-    int   bxPos      = width  / 2;
-    int   byPos      = 20;
-    float bxVel      = 6;
-    float byVel      = 6;
+    int   bxPos      = DEFAULT_BALL_X_POS;
+    int   byPos      = DEFAULT_BALL_Y_POS;
+
+    float bxVel      = DEFAULT_BALL_X_VEL;
+    float byVel      = DEFAULT_BALL_Y_VEL;
     const int radius = 20;
 
     // Platform variables
     // Start with the platform at the centre bottom of the screen.
     int pWidth      = 150;
     int pHeight     = 15;
-    int pxPos       = (width - pWidth) / 2;
-    const int pyPos = height - pHeight - 15;
+    int pxPos       = (FRAME_WIDTH - pWidth) / 2;
+    const int pyPos = FRAME_HEIGHT - pHeight - 15;
 
     int pSpeed      = 10;
 
@@ -28,13 +36,13 @@ int main()
     int highScore     = 0;
     
     // Initialize the window    
-    InitWindow(width, height, "");
-    SetWindowState(FLAG_VSYNC_HINT); // Helps prevent screen tearing
-    SetTargetFPS(target_fps);
+    InitWindow(FRAME_WIDTH, FRAME_HEIGHT, "");
+    SetTargetFPS(TARGET_FPS);
 
     while (!WindowShouldClose()){
         // Update the gamestate
         
+        // Move the ball
         bxPos += bxVel;
         byPos += byVel;
 
@@ -42,7 +50,7 @@ int main()
         bxVel *= 1.0001;
         byVel *= 1.0001;
 
-        // Platform movement
+        // Move the platform
         if (IsKeyDown(KEY_LEFT)){
             pxPos -= pSpeed;
         } else if (IsKeyDown(KEY_RIGHT)){
@@ -50,17 +58,15 @@ int main()
         }
 
         // Check for deaths
-        if (byPos >= height){
-            DrawText("GAME OVER", width * 0.3, height * 0.2 , 30, RED);
+        if (byPos >= FRAME_HEIGHT){
             if (currentScore > highScore){
                 highScore = currentScore;
             }
-
             // reset the ball position and velocities
-            bxPos = width / 2;
-            byPos = 20;
-            bxVel = 3;
-            byVel = 3;
+            bxPos = FRAME_WIDTH / 2;
+            byPos = DEFAULT_BALL_Y_POS;
+            bxVel = DEFAULT_BALL_X_VEL;
+            byVel = DEFAULT_BALL_Y_VEL;
             currentScore = 0;
         }
 
@@ -70,7 +76,7 @@ int main()
         }
     
         // Check for collisions with the left and right walls
-        if (bxPos <= 0 || bxPos >= width){
+        if (bxPos <= 0 || bxPos >= FRAME_WIDTH){
             bxVel *= -1;
         }
         
@@ -78,7 +84,7 @@ int main()
         // 1. Check that the center of the ball is within the x-range of the platform
         // 2. Check that the y pos of the ball is within the range of the platform
 
-        // FIXME this is not technically correct since it doesn't account for hitting the edge of the platform!
+        // FIXME this is crude and can be improved.
         if (bxPos          >= pxPos &&
             pxPos + pWidth >= bxPos &&
             byPos + radius >= pyPos)
@@ -95,11 +101,10 @@ int main()
         DrawCircle(bxPos, byPos, radius, RED);
         DrawRectangle(pxPos, pyPos, pWidth, pHeight, BLUE);
 
-        // Draw the UI
+        // Draw the UI in the top left hand corner of the screen
         DrawFPS(10, 20);
         DrawText(TextFormat("Current Score: %d", currentScore), 40, 40, 20, BLUE);
-        DrawText(TextFormat("High Score:    %d", highScore), 40, 60, 20, RED);
-
+        DrawText(TextFormat("High Score: %d", highScore), 40, 60, 20, RED);
         EndDrawing();
     }
     return 0;
